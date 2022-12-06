e073ef3dcf7aec9125525031be34280be65c879b
Jonathan Warburton <jonathan.warburton09@gmail.com>
2022-12-06 09:47:03 +0100

    Preserve left padding when reading input file

--- main/file_reader.py
+++ main/file_reader.py
@@ -3,6 +3,6 @@ def read(file_name: str, path: str = "main/resources") -> list:
     with open(f"{path}/{file_name}") as f:
         lines = f.readlines()
         for line in lines:
-            output.append(line.strip())
+            output.append(line.rstrip())
 
     return output
--- tests/file_reader_test.py
+++ tests/file_reader_test.py
@@ -3,6 +3,7 @@ from main import file_reader
 
 def test_file_reader():
     assert file_reader.read(file_name="test_file.txt", path="tests/resources") == [
+        " test-preserve-left-padding",
         "111",
         "222",
         "333",
